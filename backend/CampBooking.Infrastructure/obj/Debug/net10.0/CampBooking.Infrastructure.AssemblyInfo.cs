01336e156807bac582b1a4da73fb26405b82ee3b
Shivansh Dwivedi <shivanshdwivedi524@gmail.com>
2026-06-30 15:37:29 +0530

    Remove empty ci-cd workflow

--- backend/CampBooking.Infrastructure/obj/Debug/net10.0/CampBooking.Infrastructure.AssemblyInfo.cs
+++ backend/CampBooking.Infrastructure/obj/Debug/net10.0/CampBooking.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CampBooking.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c3e85b960857c0bdd72f4528f6b2caad30f8515")]
 [assembly: System.Reflection.AssemblyProductAttribute("CampBooking.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CampBooking.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
